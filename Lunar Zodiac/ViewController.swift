095a0f54b4aa27125a14b7d1b5247bf869c93df3
Jacob P Wang <59838504+jacobwang7@users.noreply.github.com>
2021-03-10 18:24:55 -0500

    Lunar Zodiac app that shows what the zodiac sign is for every year!

--- Lunar Zodiac/ViewController.swift
+++ Lunar Zodiac/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var yearLabel: UILabel!
+    @IBOutlet weak var animalImageView: UIImageView!
+    
+    var currentYear = 2021
+    var currentImageNumber = 1
     override func viewDidLoad() {
         super.viewDidLoad()
+        yearLabel.text = "\(currentYear)"
+        animalImageView.image = UIImage (named: "image\(currentImageNumber)")
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func buttonPressed(sender: UIButton) {
+        currentImageNumber = currentImageNumber + sender.tag
+        currentYear += sender.tag
+        if currentImageNumber < 0 {
+            currentImageNumber = 11
+        }
+        if currentImageNumber > 11 {
+            currentImageNumber = 0
+        }
+        yearLabel.text = "\(currentYear)"
+        animalImageView.image = UIImage(named: "image\(currentImageNumber)")
+    }
 
 }
 
